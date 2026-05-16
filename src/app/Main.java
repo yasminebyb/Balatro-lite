fcbcba1296cfce72f357b9de6f332db9b0e73062
yasminebyb <yasmine.benyoussef01@gmail.com>
2026-05-16 23:43:05 +0200

    feat() : mise a jour Main avec choix console et Zen6

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,21 +1,51 @@
 package app;
 
+import java.util.List;
+import java.util.Scanner;
+
 import controller.GameController;
 import domain.Blind;
 import domain.StandardBlind;
 import model.GameState;
 import view.ConsoleView;
-import java.util.List;
+import view.View;
+import view.Zen6View;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Blind> blinds = List.of(
-            new StandardBlind("Petit aveugle", 300),
-            new StandardBlind("Grand aveugle", 800),
-            new StandardBlind("Boss", 2000)
-        );
-        var state = new GameState(blinds, 4);
-        var view = new ConsoleView();
-        new GameController(state, view).run();
-    }
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		IO.println("=== BALATRI ===");
+		IO.println("1 - Console");
+		IO.println("2 - Interface graphique");
+
+		var choice = scanner.nextInt();
+
+		View view;
+
+		switch (choice) {
+
+		case 1 -> view = new ConsoleView();
+
+		case 2 -> view = new Zen6View();
+
+		default -> {
+			System.out.println("Choix invalide.");
+			scanner.close();
+			return;
+		}
+		}
+
+		List<Blind> blinds = List.of(new StandardBlind("Petit aveugle", 300), new StandardBlind("Grand aveugle", 800),
+				new StandardBlind("Boss", 2000));
+
+		var state = new GameState(blinds, 4);
+
+		var controller = new GameController(state, view);
+
+		controller.run();
+		scanner.close();
+	}
 }
